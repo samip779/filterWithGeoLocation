ae91699230ca54c83cd7f72f4f232e190b02088e
samip <samip@DESKTOP-MLCQOGM>
2023-03-09 17:20:04 +0545

    database configuration done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeOrmConfigAsync } from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env' }),
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
